db1387871244f217d2bec36a1cd35792f6237050
admin <rootaka@gmail.com>
2020-10-04 22:38:03 +0900

    modify the Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { logout } from '../firebase'
+import { FirebaseContext } from '../contexts'
 
 const Home = () => {
+  const { user } = useContext(FirebaseContext)
+
   return (
     <>
       <h1>Home(要ログイン)</h1>
       <div className='home'>
+        <p>{user?.email}でログインしています。</p>
         <button onClick={logout}>ログアウト</button>
       </div>
     </>
